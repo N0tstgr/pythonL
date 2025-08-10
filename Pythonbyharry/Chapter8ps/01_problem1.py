64d5a619d5e063885bd30298fdf0bd903264790c
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-10 16:46:06 +0530

    8ps

---
+++ Pythonbyharry/Chapter8ps/01_problem1.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the value of the no. : "))
+b = int(input("Enter the value of the no. : "))
+c = int(input("Enter the value of the no. : "))
+if(a >b and a>c):
+    print("this is the greatest no.",a)
+elif(b>c and b>a):
+    print("this is the greatest number .",b)
+elif(c>a and c>b):
+    print("this is the greatest number . ",c)
+   
+
