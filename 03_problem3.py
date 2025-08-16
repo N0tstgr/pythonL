3f333d1b004a592a4c99e753df10b83c3d7c8b36
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-16 12:56:44 +0530

    9ps3.2

--- 03_problem3.py
+++ 03_problem3.py
@@ -1,7 +1,10 @@
 def table():
     for num in range(2, 26):
-     for i in range(1, 11):
-      print(...)
+      filename = f"table{num}.txt"
+      with open(f"table{num}.txt", "w") as f:
+
+       for i in range(1, 11):
+        f.write(f"{num} X {i} =  {num*i}\n")
 
 
-print()   
+table()
