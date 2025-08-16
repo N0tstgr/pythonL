12681e2029375be38a7f5257bef23fdc680ee081
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-16 11:56:06 +0530

    9ps2

---
+++ 02_problem2.py
@@ -0,0 +1,29 @@
+import random
+ 
+def game():
+    print("you are playing the game ..")
+    score  = random.randint(2 , 89)
+
+    #fetch the Hiscore
+    with open("Hiscore.txt") as f:
+        Hiscore = f.read()
+        if(Hiscore!=""):
+            Hiscore = int(Hiscore)
+        else:
+            Hiscore = 0
+
+
+    print(f"your score: {score}")
+    if(score>Hiscore or Hiscore==""):
+        #write this his score to the file
+     with open("Hiscore.txt", "w") as f:
+        f.write(str(score))
+
+    
+
+
+    return score
+
+
+game()
+ 
