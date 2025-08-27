fd194840f9bf53ec5b7c8fafe216bfba2fc4d5e3
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-27 07:46:13 +0530

    11.3.1

--- 03_problem3.py
+++ 03_problem3.py
@@ -2,14 +2,10 @@ class Employee:
     def __init__(self, name,  salary):
         self.name = name
         self.salary = salary
-        
+    
 
 
-    def show(self):
-        print(f"this is the name {self.name} of the Employee and here is his salary {self.salary}")
-
-        def increment(self, percent):
-            self.salary  = self.salary * (percent/100)
 
 e = Employee("zordan", 400000)
-e.show()
+e.salary = 289
+e.increment = 90
