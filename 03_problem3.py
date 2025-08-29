b6a816cd63c12f9de9abb753debe081bf272683a
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-29 07:40:00 +0530

    11.4

--- 03_problem3.py
+++ 03_problem3.py
@@ -1,14 +1,20 @@
 class Employee:
     salary = 344
-    inrement = 90
+    increment = 90
+    @property
     def salaryAfterIncrement(self):
         return (self.salary + self.salary *(self.increment/100))
     
-    @increment.setter
-    def increment(self, salary):
-        self.increment = newsalary = oldsalary * (1 + increment/100)
+    @salaryAfterIncrement.setter
+    def salaryAfterIncrement(self, salary):
+        self.increment = ((salary/self.salary) -1)*100
 
 
 
 e = Employee()
-print(e.salaryAfterIncrement)
+# print(e.salaryAfterIncrement) 
+e.salaryAfterIncrement = 653
+print(e.increment)
+
+
+##visit it again that is not clear
