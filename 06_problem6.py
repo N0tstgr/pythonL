8fadd06126d442e48f69dc3e948e620079e9b8d0
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-30 09:57:54 +0530

    06

---
+++ 06_problem6.py
@@ -0,0 +1,26 @@
+import sympy as sp
+i, j, k = sp.symbols('i j k')
+
+class Vector:
+    def __init__(self, x, y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+    def __mul__(self, other):
+        result =  Vector({self.x * other.x + self.y * other.y + self.z * other.z})
+        return result 
+        
+        
+
+
+
+
+    def __str__(self):
+        return f"({self.x}, {self.y}, {self.z})"
+    
+
+v1 = Vector(7,8,10)
+
+print(v1*v2)
